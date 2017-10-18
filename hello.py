df4bb0c02a1c074310c0a73a4813c103266ae775
CHENJIAYI <chenjy0621@gmail.com>
2017-10-18 20:59:43 +0100

    I have commited my file the third time

--- hello.py
+++ hello.py
@@ -1,8 +1,5 @@
 print("Hello")
 print("World")
 print("More changes")
-<<<<<<< HEAD
 print("Still more changes")
-=======
-print("still more changes")
->>>>>>> facdd4bfe9a402f0ca1909606e51deeaf1ad57ce
+
